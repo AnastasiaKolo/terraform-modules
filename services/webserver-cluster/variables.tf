4a0fd5f8e0363dec9ce63ad2363ee1582569ac2c
AnastasiaKolo <kolodochko@gmail.com>
2020-11-26 21:34:57 +0300

    chapter5 custom tags

--- services/webserver-cluster/variables.tf
+++ services/webserver-cluster/variables.tf
@@ -1,3 +1,10 @@
+variable "custom_tags" {
+  description = "Custom tags to set on the Instances in the ASG"
+  type        = map(string)
+  default     = {}
+}
+
+
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
   type        = number
