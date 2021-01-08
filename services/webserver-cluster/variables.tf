b4782501c4431f37dbe45ddc03cde6e4f1142cd7
AnastasiaKolo <kolodochko@gmail.com>
2021-01-08 13:11:27 +0300

    5 chapter conditionals

--- services/webserver-cluster/variables.tf
+++ services/webserver-cluster/variables.tf
@@ -1,3 +1,8 @@
+variable "enable_autoscaling" {
+  description = "If set to true, enable auto scaling"
+  type        = bool
+}
+
 variable "custom_tags" {
   description = "Custom tags to set on the Instances in the ASG"
   type        = map(string)
